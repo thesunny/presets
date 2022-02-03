76df5c5b8ac4e863ea267e64126419cf8c13f039
Sunny Hirai <thesunny@gmail.com>
2022-02-03 12:45:27 -0800

    Improve Next.js preset

--- presets/next/index.ts
+++ presets/next/index.ts
@@ -5,16 +5,35 @@ import * as core from "mrm-core"
 
 export default function nextPreset() {
   utils.title(`Preset next`)
+
+  /**
+   * Get ts-jest and all the typescript stuff set up
+   */
   tsJestPreset()
+
+  /**
+   * Get the additional required `tsconfig` files for Next.js
+   */
   utils.copyLocalFiles([
     "tsconfig.base.json",
     "tsconfig.custom.json",
-    "tsconfig.json",
+    "tsconfig.json", // This is the Next.js tsconfig
   ])
+
+  /**
+   * Add default page
+   */
   utils.copyLocalFiles(["pages/index.tsx"], { exists: "skip" })
+
+  /**
+   * Add dependencies
+   */
   utils.addDeps(["next", "react", "react-dom"])
   utils.addDevDeps(["@types/react", "@types/react-dom"])
 
+  /**
+   * Add script if one doesn't exist for `next`
+   */
   utils.heading("Add start:dev script")
   const pkg = core.packageJson()
   const startDevScript = pkg.getScript("start:dev")
